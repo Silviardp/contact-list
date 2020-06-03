69d29570ca50be4b989aaab85aa95a654d0a6d90
Silvia Rocha <silviadprocha@gmail.com>
2020-06-03 23:07:05 +0900

    add api to fetch random users

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ContactCard from "./components/ContactCard";
 
 const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  fetch("https://randomuser.me/api/?results=3")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setContacts(data.results);
+    });
+
   return (
     <div>
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jenny Han"
-        email="jenny.han@notreal.com"
-        age={25}
-      />
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jason Long"
-        email="jason.long@notreal.com"
-        age={45}
-      />
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Peter Pan"
-        email="peter.pan@neverland.com"
-        age={100}
-      />
+      {contacts.map((contact) => (
+        <ContactCard
+          avatar={contact.picture.large}
+          name={contact.name.first + " " + contact.name.last}
+          email={contact.email}
+          age={contact.dob.age}
+        />
+      ))}
     </div>
   );
 };
