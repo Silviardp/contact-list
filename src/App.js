ee3bf5e0a3b9314e20bc2dc8f3376b5299eb614a
Silvia Rocha <silviadprocha@gmail.com>
2020-06-03 19:40:21 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import ContactCard from "./components/ContactCard";
 const App = () => {
   return (
     <div>
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Jenny Han"
+        email="jenny.han@notreal.com"
+        age={25}
+      />
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Jason Long"
+        email="jason.long@notreal.com"
+        age={45}
+      />
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Peter Pan"
+        email="peter.pan@neverland.com"
+        age={100}
+      />
     </div>
   );
 };
